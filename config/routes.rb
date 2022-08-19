fda837ea760816eeac9949a028f278b0347b9ca8
Vinny Algaron <vinny-algaron93@hotmail.fr>
2022-08-19 12:00:02 +0200

    routes updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  root to: "lists#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: [:new, :destroy]
 end
