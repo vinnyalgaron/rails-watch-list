4ec5183de7831b3fea5ae90e6855bd5b453451b2
Vinny Algaron <vinny-algaron93@hotmail.fr>
2022-08-19 14:31:46 +0200

    controller lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,23 @@
 class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+
+
+
+
+
+
+
+
+
 end
