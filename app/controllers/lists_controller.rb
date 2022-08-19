f8606895c7ab6da0cb4d3323f5788540626afb52
Vinny Algaron <vinny-algaron93@hotmail.fr>
2022-08-19 14:46:17 +0200

    controller model

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -11,13 +11,18 @@ class ListsController < ApplicationController
     @list = List.new
   end
 
+  def create
+    @list = List.new(list_params)
 
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :see_other
+    end
+  end
 
-
-
-
-
-
-
-
+  private
+  def list_params
+    params.require(:list).permit(:name, :banner_url)
+  end
 end
